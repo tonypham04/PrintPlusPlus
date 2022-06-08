eeda202220221600a2852aeae41f653492954a56
Tony Pham <tonyphamx04@gmail.com>
2022-06-08 18:17:03 -0400

    Add example of clicking a button and showing callback function output in a label

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from tkinter import Tk
 from tkinter import ttk
+from tkinter import StringVar
 
 # Create the main window of the application
 root = Tk()
-frame = ttk.Frame(root, padding=10)
-frame.grid()
-ttk.Label(frame, text='Hello World').grid(column=0, row=0)
-ttk.Button(frame, text='Quit', command=root.destroy).grid(column=1, row=0)
+# Create widgets
+frm = ttk.Frame(root, padding=10)
+
+content_frm = ttk.Frame(frm, padding=10)
+results_txt = StringVar()
+output_lbl = ttk.Label(content_frm, textvariable=results_txt)
+run_btn = ttk.Button(content_frm, text='Run', command=lambda: results_txt.set(run_btn.configure().keys()))
+
+footer_frm = ttk.Frame(frm, padding=10)
+quit_btn = ttk.Button(footer_frm, text='Quit', command=root.destroy)
+
+# Place widgets
+frm.grid()
+
+content_frm.grid(row=0, column=0)
+run_btn.grid(row=0, column=0)
+output_lbl.grid(row=1, column=0)
+
+footer_frm.grid(row=1, column=0)
+quit_btn.grid(row=0, column=0)
+
 root.mainloop()
+
+#TODO: https://docs.python.org/3/library/tkinter.html#important-tk-concepts
