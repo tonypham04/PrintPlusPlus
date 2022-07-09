d88d9699a70923a8d000986a491fa839a808e56a
Tony Pham <tonyphamx04@gmail.com>
2022-07-09 14:02:25 -0400

    Refactor parameters and assignment operators to contain spaces

--- app.py
+++ app.py
@@ -17,33 +17,33 @@ root.iconbitmap('hourglass')
 root.resizable(False, False)
 
 # Create widgets
-frm = ttk.Frame(root, padding=10)
+frm = ttk.Frame(root, padding = 10)
 
-button_frm = ttk.Frame(frm, padding=10)
-run_btn = ttk.Button(button_frm, text='\U00002BC8 Run', command=lambda: update_output_text(output_text, run(run_btn.configure().keys())))
-reset_btn = ttk.Button(button_frm, text='\U0001F5D8 Reset', command=lambda: reset_output_text(output_text))
+button_frm = ttk.Frame(frm, padding = 10)
+run_btn = ttk.Button(button_frm, text = '\U00002BC8 Run', command = lambda: update_output_text(output_text, run(run_btn.configure().keys())))
+reset_btn = ttk.Button(button_frm, text = '\U0001F5D8 Reset', command = lambda: reset_output_text(output_text))
 export_btn = ttk.Button(button_frm, text = '\U0001F4BE Export')
 
 content_frm = ttk.Frame(frm, padding=10)
 results_txt = StringVar()
-output_text = Text(content_frm, state='disabled', wrap='word', bg='#D3D3D3')
+output_text = Text(content_frm, state = 'disabled', wrap = 'word', bg = '#D3D3D3')
 
-footer_frm = ttk.Frame(frm, padding=10)
-quit_btn = ttk.Button(footer_frm, text='Quit', command=root.destroy)
+footer_frm = ttk.Frame(frm, padding = 10)
+quit_btn = ttk.Button(footer_frm, text = 'Quit', command = root.destroy)
 
 # Place widgets
 frm.grid()
 
-button_frm.grid(row=0, column=0, sticky='w')
-run_btn.grid(row=0, column=0)
-reset_btn.grid(row=0, column=1, padx=10)
+button_frm.grid(row = 0, column = 0, sticky = 'w')
+run_btn.grid(row = 0, column = 0)
+reset_btn.grid(row = 0, column = 1, padx = 10)
 export_btn.grid(row = 0, column = 2)
 
-content_frm.grid(row=1, column=0)
-output_text.grid(row=1, column=0)
+content_frm.grid(row = 1, column = 0)
+output_text.grid(row = 1, column = 0)
 
-footer_frm.grid(row=2, column=0)
-quit_btn.grid(row=0, column=0)
+footer_frm.grid(row = 2, column = 0)
+quit_btn.grid(row = 0, column = 0)
 
 root.mainloop()
 
