38280b318a6e280faa735838a42d60182a9104f8
Tony Pham <tonyphamx04@gmail.com>
2022-10-01 09:13:19 -0400

    Add documentation for newly added classes and non-private functions

--- buttonstylefactory.py
+++ buttonstylefactory.py
@@ -1,15 +1,16 @@
 from customstyle import CustomStyle
 
 class ButtonStyle(CustomStyle):
-
+    """A CustomStyle with a name attribute which aligns with naming for a ttk.Button."""
     def __init__(self, name: str):
         super().__init__(name)
         self.name = f'{name}.TButton'
 
 class ButtonStyleFactory:
-
+    """Controls the instantiation of ButtonStyle objects."""
     @staticmethod
-    def get_style(name: str):
+    def get_style(name: str) -> ButtonStyle:
+        """Returns a ButtonStyle based on the requested palette."""
         button_style = ButtonStyle(name)
         if name.lower() == 'coolblue':
             ButtonStyleFactory._set_styles(button_style, 'blue', 'lightblue')
--- stylemanager.py
+++ stylemanager.py
@@ -5,8 +5,10 @@ from framestylefactory import FrameStyleFactory
 from tkinter.ttk import Widget
 
 class StyleManager:
+    """Handle the assignment of styles to Widgets."""
     @staticmethod
     def apply_styles(type: str, name: str, parent: Frame, use_primary_color = False):
+        """Apply styles to a specified Widget type based on a specified style."""
         if type.lower() == 'button':
             btn_style = ButtonStyleFactory.get_style(name)
             StyleManager._set_styles(btn_style, parent)
@@ -21,11 +23,13 @@ class StyleManager:
 
     @staticmethod
     def remove_child_styles(parent: Frame):
+        """Removes all styles from all child widgets in a frame."""
         for child in parent.winfo_children():
             StyleManager.remove_widget_styles(child)
 
     @staticmethod
     def remove_widget_styles(widget: Widget):
+        """Removes all styles from the specified widget."""
         widget.configure(style = widget.winfo_class())
 
 # tcl color reference: https://tcl.tk/man/tcl8.6/TkCmd/colors.htm
